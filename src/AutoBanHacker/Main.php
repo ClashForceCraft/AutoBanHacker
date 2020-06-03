d6c78449c4b6afcb11024d1e8091ea334e00030b
ClashForceCraft <66352046+ClashForceCraft@users.noreply.github.com>
2020-06-03 22:06:20 +0800

    Update Main.php

--- src/AutoBanHacker/Main.php
+++ src/AutoBanHacker/Main.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace AutoBanHacker;
+
+use pocketmine\Server;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\{Command, CommandSender};
+
+class Main extends PluginBase {
+
+       public function onEnable(){
+
+       }
+
+      
+
+       public function onCommand(CommandSender $player, Command $cmd, string $label, array $args) :bool {
+
+      
+
+             switch($cmd->getName()}{
+
+             case "abh help":
+
+              if($player instanceof Player){
+
+                    $player->sendMessage("§eAutoBanHelp: /abh enable");
+
+                   
+
+             switch($cmd->getName()}{
+
+             case "abh on":
+
+              if($player instanceof Player){
+
+                    $player->sendMessage("§aAutoBanHacker ON");
+
+            } 
+
+            break;
+
+            } 
+
+            return true;
+
+           
+
+      }
